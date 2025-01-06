34873e7e5f9114efaab213a0c38c7a8be8777f2b
STEPHTRUBETSKOFF <stephaniesnowflaketrubetskoff@gmail.com>
2025-01-06 15:54:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be: ', name_on_order)
 
-conn = st.connection("snowflake")
-session = conn.session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
