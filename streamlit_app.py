f7f7f07641581bff424c5accf23df90826425cd4
STEPHTRUBETSKOFF <stephaniesnowflaketrubetskoff@gmail.com>
2025-01-06 15:08:15 -0700

    Update streamlit_app.py
    updated title and added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,18 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!
+    """Breakfast Menu
+    """)
+st.write(
+    """Omega 3 & Bluberry Oatmeal
+    """)
+st.write(
+    """Kale, Spinach & Rocket Smoothie
+    """)
+st.write(
+    """Hard-Boiled Free-Range Egg
     """)
 
 name_on_order = st.text_input('Name on Smoothie:')
